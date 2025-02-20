fee42b54d8a42ec3daab716738b0715fad0ee7e7
natasyakaramazov <kierraashford@gmail.com>
2025-02-19 18:45:15 -0600

    Completed Rook.java and reviewed Pawn.java for understanding

--- src/main/Pawn.java
+++ src/main/Pawn.java
@@ -23,5 +23,24 @@ public class Pawn extends Piece {
 		}
 		
 		
+		// If you want to move two squares 
+		
+		boolean isStartingPosition = (color == PieceColor.WHITE && position.getRow())
+		 == 6 || (color == PieceColor.BLACK && position.getRow() == 1);
+		
+		
+		if(colDiff == 0 && rowDiff == 2 && isStartingPosition && board[newPosition.getRow()][newPosition.getColumn()] == null) {
+			
+			 //check if there's something blocking the way
+			int middleRow = position.getRow() + forwardDirection;
+			if(board[middleRow][position.getColumn()] == null) {
+				return true;
+			}
+		
+		}//Diagonal capture
+		if (Math.abs(colDiff) == 1 && rowDiff == 1 && board[newPosition.getRow()][newPosition.getColumn()] != null && board[newPosition.getRow()][newPosition.getColumn()]!= this.color) {
+			return true;
+		}
+		return false;
 	}
 }
--- src/main/Rook.java
+++ src/main/Rook.java
@@ -1,1 +1,40 @@
 package com.ashford.javachessgame;
+public class Rook extends Piece{
+	public Rook(PieceColor color, Positin position) {
+		super(color,position);
+	}
+	@Override
+	public boolean isValidMove(Position newPosition, Piece[][] board) {
+		//Rooks are 5 points and can move vertically or horizontally any number of spaces
+		if(position.getRow() == newPosition.getRow()) {
+			int columnStart = Math.min(position.getColumn(), newPosition.getColumn()) + 1;
+			int columnEnd = Math.max(position.getColumn(), newPosition.getColumn());
+			 fo(int column = columnStart; column <columnEnd; column++){
+				 if(board[position.getRow()][column]!= null) {
+					 return false; //Checking to see if there's a piece blocking the destination
+				 }
+				 
+			 }
+		}
+		else if(position.getColumn()  == newPosition.getColumn()) {
+			int rowStart = Math.min(position.getRow(),newPosition.getRow() + 1 );
+			int rowEnd = Math.max(position.getRow(), newPosition.getRow());
+			for(int row = rowStart, row < rowEnd; row++) {
+				 if(board[row][position.getColumn()]!=null) {
+					 return false;  //something blocking the way vertically
+				 }
+			}
+		}else {
+			return false;
+		}
+		//capture possible or not in new destination 
+	    Piece destinationPiece = board[newPosition.getRow()][newPosition.getColumn];
+	    	if(destinationPiece == null) {
+	    		return true; //empty space, go ahead
+	    	} else if(destinationPiece.getColor()!= this.getColor()) {
+	    		return true; //capture available 
+	    	}
+	    	return false; //invalid move overall
+	   
+	}
+}
